f4c780d4c77259ae9ccc48694162ed3143b5316b
Hugh <htcolema@uwaterloo.ca>
2026-03-12 16:34:55 -0400

    more

--- Fettuccine/GroebnerBasis/MvPolynomial/CMonomialOrder.lean
+++ Fettuccine/GroebnerBasis/MvPolynomial/CMonomialOrder.lean
@@ -1,20 +1,41 @@
 import Fettuccine.GroebnerBasis.MvPolynomial.CMvPolynomial
 
+import Mathlib.Data.Finsupp.Lex
+import Mathlib.Data.Finsupp.WellFounded
+import Mathlib.Data.List.TFAE
+
 /-!
 # Monomial orders
 
-This file defines computable monomial orders and provides the instances `Lex`,
-`Grlex`, and `Grevlex`.
+This file defines computable monomial orders and provides the orders `Lex`,
+`Grlex`, and `Grevlex` on monomials.
 -/
+
+/-- A computable total order on `CMonomial σ`. -/
+structure CMonomialOrder (σ : Type*) [LinearOrder σ] where
+  lt : CMonomial σ → CMonomial σ → Bool
 
 namespace MonomialOrder
 
-/-- A computable total order on `CMonomial σ`.  -/
-structure CMonomialOrder (σ : Type*) where
-  lt : CMonomial σ → CMonomial σ → Bool
+variable {σ : Type*} [LinearOrder σ]
 
--- TODO:
--- [ ] Proof that a CMonomialOrder is a MonomialOrder.
+def le (m : CMonomialOrder σ) (a b : CMonomial σ) : Bool :=
+  m.lt a b || a = b
 
+/-- Notation for the ordering relation on `CMonomial σ`. -/
+notation:50 lhs " ≺[" m:25 "] " rhs:50 => (CMonomialOrder.lt  m lhs rhs)
+notation:50 lhs " ≼[" m:25 "] " rhs:50 => (CMonomialOrder.lte m lhs rhs)
 
 end MonomialOrder
+
+/-- The lexicographic order on monomials. -/
+instance lex (σ : Type*) [LinearOrder σ] : CMonomialOrder σ where
+  lt a b := sorry
+
+/-- The graded lexicographic order on monomials. -/
+instance grlex (σ : Type*) [LinearOrder σ] : CMonomialOrder σ where
+  lt a b := sorry
+
+/-- The graded reverse lexicographic order on monomials. -/
+instance grevlex (σ : Type*) [LinearOrder σ] : CMonomialOrder σ where
+  lt a b := sorry
--- TODO.md
+++
@@ -1,3 +0,0 @@
-# TODO
-
-- Syntax: `in[m]`, `<[m]`, and `\leq[m]` for initial ideal and comparisons of monomials with respect to a particular monomial order `m`.
